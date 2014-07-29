ed0e72f9539c9556616813f8e06dce4186de8e81
Antoine Hedgecock <antoine.hedgecock@gmail.com>
2014-07-29 16:03:04 +0200

    Did the same to the PDOStatement class.

--- test/CrateTest/PDO/PDOStatementTest.php
+++ test/CrateTest/PDO/PDOStatementTest.php
@@ -3,21 +3,47 @@
 
 namespace CrateTest\PDO;
 
+use Crate\PDO\Exception\UnsupportedException;
 use Crate\PDO\PDOStatement;
 use PHPUnit_Framework_TestCase;
 
 /**
  * Tests for {@see \Crate\PDO\PDOStatement}
  *
- * @covers \Crate\PDO\PDOStatement
+ * @coverDefaultClass \Crate\PDO\PDOStatement
+ *
+ * @group unit
+ * @group statement
  */
 class PDOStatementTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @var PDOStatement
+     */
+    protected $statement;
+
+    protected function setUp()
+    {
+        $this->statement = new PDOStatement();
+    }
+
+    /**
+     * @covers ::__construct
+     */
     public function testInstantiaton()
     {
         $pdoStatement = new PDOStatement();
 
         $this->assertInstanceOf('Crate\PDO\PDOStatement', $pdoStatement);
         $this->assertInstanceOf('PDOStatement', $pdoStatement);
+    }
+
+    /**
+     * @covers ::closeCursor
+     */
+    public function testCloseCursorThrowsUnsupportedException()
+    {
+        $this->setExpectedException(UnsupportedException::class);
+        $this->statement->closeCursor();
     }
 }
