7bfb927e30b2c6c8b303efea35585bac0e592044
Antoine Hedgecock <antoine.hedgecock@gmail.com>
2014-08-20 13:16:56 +0200

    Added HHVM to the provisioning

--- provisioning.sh
+++ provisioning.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+wget -O - http://dl.hhvm.com/conf/hhvm.gpg.key | sudo apt-key add -
+echo deb http://dl.hhvm.com/ubuntu trusty main | sudo tee /etc/apt/sources.list.d/hhvm.list
 sudo apt-get install -y python-software-properties
 sudo add-apt-repository -y ppa:crate/stable
 sudo apt-get update
-sudo apt-get install -y crate php5 php5-xdebug
+sudo apt-get install -y crate hhvm php5 php5-xdebug
