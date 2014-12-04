8cf12a8155064e27e34ec06b10f13300cffee48b
Sebastian Utz <su@rtme.net>
2014-12-04 16:34:33 +0100

    prepare new release 0.0.6

--- src/Crate/PDO/PDO.php
+++ src/Crate/PDO/PDO.php
@@ -28,7 +28,7 @@ use PDO as BasePDO;
 
 class PDO extends BasePDO implements PDOInterface
 {
-    const VERSION = '0.0.5';
+    const VERSION = '0.0.6';
     const DRIVER_NAME = 'crate';
 
     const DSN_REGEX = '/^(?:crate)?(?::([\w\d\.-]+:\d+))+/';
