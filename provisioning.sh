a47fa14876b8aed13198eb77d330656403055763
Antoine Hedgecock <antoine.hedgecock@gmail.com>
2014-12-27 16:25:54 +0100

    This should resolve the error about python-software-properties not existing

--- provisioning.sh
+++ provisioning.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 wget -O - http://dl.hhvm.com/conf/hhvm.gpg.key | sudo apt-key add -
 echo deb http://dl.hhvm.com/ubuntu trusty main | sudo tee /etc/apt/sources.list.d/hhvm.list
+sudo apt-get update
 sudo apt-get install -y python-software-properties
 sudo add-apt-repository -y ppa:crate/stable
 sudo apt-get update
