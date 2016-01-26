23e73c2da046b74ef8e60b3a2a688b9adc5dcac7
Chris Ward <chriswhward@gmail.com>
2016-01-26 16:09:30 +0100

    config change for new urls

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,6 @@ from crate.theme.rtd.conf import *
 project = u'Crate PDO'
 source_suffix = '.rst'
 html_theme_options.update({
-    'canonical_url_path': 'docs/projects/crate-pdo/',
+    'canonical_url_path': 'docs/reference/pdo/',
     'tracking_project': 'crate-pdo',
 })
-
