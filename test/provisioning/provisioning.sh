beb28466f7bf71d2d231915e595331b6b1dd9e92
Fabian Reisegger <fabian.reisegger@students.fhv.at>
2018-04-03 09:16:23 +0200

    added missing dependency and fixed crate.yml path in test/provisioning.sh

--- test/provisioning/provisioning.sh
+++ test/provisioning/provisioning.sh
@@ -5,10 +5,10 @@ apt-get install -y python-software-properties
 add-apt-repository -y ppa:ondrej/php
 apt-get update
 bash -c "$(curl -L install.crate.io)"
-apt-get install -y php7.2-cli php7.2-xml php7.2-curl
+apt-get install -y php7.2-cli php7.2-xml php7.2-curl php7.2-mbstring
 service crate stop
 rm /etc/crate/crate.yml
-ln -s /vagrant/provisioning/crate.yml /etc/crate/crate.yml
+ln -s /vagrant/test/provisioning/crate.yml /etc/crate/crate.yml
 echo "127.0.0.1 ssl.crate.io" >> /etc/hosts
 mkdir /data
 chown crate:crate /data
