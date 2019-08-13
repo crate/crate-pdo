24bd379f4ea1fb047cf51c3c659d62012d7c5904
Sebastian Utz <su@rtme.net>
2019-08-13 15:35:34 +0200

    Fix CrateDB setup used for testing, adds jdk 11
    Since CrateDB >= 4.0 java versions >= 11 are required.
    Also switched installing CrateDB from install.crate.io to
    use the latest version of the `stable` ubuntu channel.

--- test/provisioning/provisioning.sh
+++ test/provisioning/provisioning.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
+apt-get upgrade
 apt-get install -y python-software-properties
+add-apt-repository -y ppa:openjdk-r/ppa
 add-apt-repository -y ppa:ondrej/php
+wget https://cdn.crate.io/downloads/deb/DEB-GPG-KEY-crate
+apt-key add DEB-GPG-KEY-crate
+. /etc/os-release
+echo "deb https://cdn.crate.io/downloads/deb/stable/ $UBUNTU_CODENAME main" > /etc/apt/sources.list.d/crate-stable-$UBUNTU_CODENAME.list
 apt-get update
-bash -c "$(curl -L install.crate.io)"
-apt-get install -y php7.2-cli php7.2-xml php7.2-curl php7.2-mbstring
+apt-get install -y openjdk-11-jre-headless crate php7.2-cli php7.2-xml php7.2-mbstring php7.2-xdebug
 service crate stop
 rm /etc/crate/crate.yml
 ln -s /vagrant/test/provisioning/crate.yml /etc/crate/crate.yml
