82e17ee806e39b0ce964ee880b674a6a543016bb
Naomi Slater <23469+mechanomi@users.noreply.github.com>
2020-04-29 17:24:38 +0200

    Config update for crate-docs-theme 0.9.0

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,4 @@
-from crate.theme.rtd.conf.clients_pdo import *
+from crate.theme.rtd.conf.pdo import *
 
 from sphinx.highlighting import lexers
 from pygments.lexers.web import PhpLexer
@@ -8,9 +8,9 @@ lexers["php-annotations"] = PhpLexer(startinline=True, linenos=1)
 
 source_suffix = '.rst'
 html_theme_options.update({
-    'canonical_url_path': 'docs/reference/pdo/',
+    'canonical_url_path': 'docs/pdo/',
     'tracking_project': 'crate-pdo',
 })
 
-site_url = 'https://crate.io/docs/clients/pdo/en/latest/'
+site_url = 'https://crate.io/docs/pdo/en/latest/'
 extensions = ['sphinx_sitemap']
