f357f5026b72248394499ce6a1d43e16b1adfa5e
Naomi Slater <23469+mechanomi@users.noreply.github.com>
2020-05-07 15:48:17 +0200

    Prune config (no longer needed)

--- docs/conf.py
+++ docs/conf.py
@@ -5,12 +5,3 @@ from pygments.lexers.web import PhpLexer
 lexers["php"] = PhpLexer(startinline=True, linenos=1)
 lexers["php-annotations"] = PhpLexer(startinline=True, linenos=1)
 # primary_domain = "php"
-
-source_suffix = '.rst'
-html_theme_options.update({
-    'canonical_url_path': 'docs/pdo/',
-    'tracking_project': 'crate-pdo',
-})
-
-site_url = 'https://crate.io/docs/pdo/en/latest/'
-extensions = ['sphinx_sitemap']
