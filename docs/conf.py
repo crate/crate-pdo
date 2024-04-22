051433d9fba99aba40a1d1d94d320038b6c494de
Matthias <matthias@crate.io>
2024-04-22 14:37:25 +0200

    disable leftover version chooser

--- docs/conf.py
+++ docs/conf.py
@@ -5,3 +5,10 @@ from pygments.lexers.web import PhpLexer
 lexers["php"] = PhpLexer(startinline=True, linenos=1)
 lexers["php-annotations"] = PhpLexer(startinline=True, linenos=1)
 # primary_domain = "php"
+
+# Disable version chooser.
+html_context.update({
+    "display_version": False,
+    "current_version": None,
+    "versions": [],
+})
