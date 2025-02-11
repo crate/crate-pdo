eb30e9767312487d29ac3c2707db53f9370cf672
Micha(el) Bladowski <info@macropage.de>
2025-02-11 12:58:07 +0100

    PHP 8.4: Fix deprecation warnings

--- src/Crate/PDO/Exception/UnsupportedException.php
+++ src/Crate/PDO/Exception/UnsupportedException.php
@@ -28,7 +28,7 @@ use Exception;
 
 class UnsupportedException extends PDOException
 {
-    public function __construct($message = 'Unsupported functionality', $code = 0, Exception $previous = null)
+    public function __construct($message = 'Unsupported functionality', $code = 0, ?Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
